c3be582e101c32c97a98bfb79f65848f93cf59f9
Kurler3 <miguelbento2000@gmail.com>
2021-03-22 19:13:45 +0800

    Some more changes

--- traversytediatutorial/src/App.js
+++ traversytediatutorial/src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState([
   {
@@ -39,10 +40,20 @@ function App() {
     {...task, reminder : !task.reminder} : task))
   }
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+
+    const newTask = {id, ...task}
+
+    setTasks([...tasks, newTask])
+  }
+
+
   return (
     <div className="container">
       <Header/>
-      <AddTask/>
+      {showAddTask ? <AddTask onAdd={addTask}/> : ''}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
